967c8007961a7212bdddbf324d25a228b51baa6a
Dmitryi Zhilkin <spomidor8@gmail.com>
2023-09-22 20:46:32 +0300

    first com

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import NavBar from "./section/NavBar";
+
+function App() {
+  return (
+    <main>
+      <NavBar />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/section/NavBar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import search from "../assets/icons/zoom.png";
+import logoNav from "../assets/img/logoNav.avif";
+import faccebok from "../assets/icons/faccebook.png";
+import inst from "../assets/icons/inst.png";
+import yt from "../assets/icons/youtube.png";
+import cart from "../assets/icons/cart.png";
+
+const NavBar = () => {
+  return (
+    <header>
+      <section className=" mt-4 flex  max-container  items-center justify-between">
+        <div className="flex hover:scale-105 cursor-pointer">
+          <img width={30} height={30} src={search} alt="search" />
+        </div>
+        <div className=" flex ">
+          <img src={logoNav} alt="logoNav" />
+        </div>
+        <div className="flex  items-center justify-around  ">
+          <img
+            className="m-3"
+            width={35}
+            height={35}
+            src={faccebok}
+            alt="faccebok"
+          />
+          <img className="m-1" width={35} height={35} src={inst} alt="inst" />
+          <img className="m-2" width={35} height={35} src={yt} alt="yt" />
+          <img className="m-1" src={cart} width={45} height={45} alt="cart" />
+        </div>
+      </section>
+      <section>
+        <div className="w-full h-16 bg-gray-200 flex justify-center items-center  ">
+          <div className="flex justify-between items-center w-[650px] text-xl">
+            <a className="" href="">
+              {" "}
+              Home
+            </a>
+            <a href=""> Shop</a>
+            <a href="">About</a>
+            <a href=""> Blog</a>
+            <a href="">Faq</a>
+            <a href=""> Contact</a>
+          </div>
+        </div>
+      </section>
+    </header>
+  );
+};
+
+export default NavBar;
